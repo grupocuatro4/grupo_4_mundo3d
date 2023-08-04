7f32cc46ec8b9920863219914e372837e3a81c04
Jorge Federico Urich <federicourichlc@gmail.com>
2023-08-04 20:43:51 -0300

    Urich Federico

---
+++ clase4/2.js
@@ -0,0 +1,24 @@
+/*1. En la carpeta ya creada crea un nuevo archivo JavaScript (.js).
+2. Desarrolla el programa Ajuste tarifario de energía eléctrica. Utilizando el operador
+condicional ternario, queremos determinar cuánto va a pagar un hogar según su
+consumo. Veamos los pasos a seguir...
+a. Crear una variable “pagoMes” y asignarle un pago actual de energía
+eléctrica por mes.
+b. Crear otra variable “consumoKWH” y asignarle un consumo mensual de
+Kilovatios hora de consumo mensual por el hogar.
+c. Si en el hogar se consumen más de 300 kwh por mes, entonces al pago actual
+se le incrementará un 20%.
+d. En el caso contrario se le mantendrá el subsidio, es decir que no tendrá ningún
+aumento..
+
+3. Una vez obtenido el monto del pago por consumo mostrar al usuario este mensaje:
+
+“Debido a que su hogar tuvo un consumo de 450kwh, en base al ajuste tarifario
+(hogares con consumo mayor a 300kwh por mes tendrán un aumento del 20%),
+cumplimos con informarle que se ha ajustado el total a pagar, que será de $14400” */
+
+let pagoMes= 3500;
+let consumoKWH= 350;
+let pagoActual=0;
+consumoKWH>300?pagoActual=((pagoMes*30)/100)+ pagoMes:pagoActual=pagoMes;
+console.log(pagoActual);
---
+++ clase4/4.js
@@ -0,0 +1,14 @@
+let number1=1;
+let number2=3;
+let operation="-";
+switch(operation){
+    case "+":console.log(number1 + number2);
+    break;
+    case "-":console.log(number1 - number2);
+    break;
+    case "*":console.log(number1 * number2);
+    break;
+    case "/":console.log(number1 / number2);
+    break;
+    
+}
