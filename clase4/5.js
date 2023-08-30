b80cf77d9bb0de3c365cdce72d6277c8e06fe399
Gino Lencina <84538965+GinoL221@users.noreply.github.com>
2023-08-30 19:55:59 -0300

    Node y nodemon

--- clase4/5.js
+++
@@ -1,5 +0,0 @@
-let avionVel= 270;
-let avionAlt=150;
-
-let aterrizarAvion= (avionVel >=268 && avionVel<=278) && (avionAlt>=150 && avionAlt<=300);
-aterrizarAvion? console.log("El avion tiene permiso de aterrizar"):console.log("El avion no puede aterrizar");
